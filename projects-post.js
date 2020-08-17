a66873a67930edec924c5117c522c7e7406efea3
Andre Freeman <asf@mail.usf.edu>
2020-08-17 18:38:39 -0400

    created get project by ID endpoint

--- projects-post.js
+++ projects-post.js
@@ -3,6 +3,21 @@ const projects = require("./data/helpers/projectModel");
 const actions = require("./data/helpers/actionModel");
 const projectRouter = express.Router();
 
+//Get Project by ID
 
+projectRouter.get("/projects/:id", (req, res, next) => {
+  projects
+    .get(req.params.id)
+    .then(project => {
+      if (project) {
+        res.status(200).json(project);
+      } else {
+        res.status(404).json({
+          message: "The project with the specified ID does not exist."
+        });
+      }
+    })
+    .catch(next);
+});
 
 module.exports = projectRouter;
